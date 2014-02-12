4dcb2f0dea3e27d05c13dbd6cc7052d22f63c5a8
lucasmd <lucas.mirandedavid@iut-valence.fr>
2014-02-12 15:52:13 +0100

    version initiale des fichiers
    Création d'un projet Eclipse pour l'exemple de la lampe

---
+++ src/Bulb.java
@@ -0,0 +1,29 @@
+
+public class Bulb
+{
+	{ 
+		public int DEFAULT_POWER = 100;
+		private static int power
+		private boolean isLit
+	
+		}
+	{
+		{
+			public void switchOnMethod()
+			
+		}
+		{
+			public void switchOffMethod()
+			?
+		}
+		{
+			public int getPower()
+			
+		}
+		{ 
+			public boolean isLit();
+		}
+		
+		
+	}
+}
